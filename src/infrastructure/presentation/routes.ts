727919a753cb7395c24bc8aed4099907d12b7995
Matheus Braga <matheus.braga.work@gmail.com>
2020-09-01 19:23:49 -0300

    :fire: Remove unexistent import.

--- src/infrastructure/presentation/routes.ts
+++ src/infrastructure/presentation/routes.ts
@@ -1,9 +1,5 @@
 import { Router } from 'express';
 
-import userRoutes from '../../modules/user/infrastructure/presentation/routes';
-
 const router = Router();
-
-router.use(userRoutes);
 
 export default router;
