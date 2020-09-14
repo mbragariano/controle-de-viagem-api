6620f38a5d5e600a5eb65b901942b77c73cbe9c5
Matheus Braga <49031528+mbragariano@users.noreply.github.com>
2020-09-13 22:11:38 -0300

    :pencil2: Add emoji on initial log.

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 import application from './src/shared/infra/presentation/server';
 
-application.listen(3000, () => console.log('Server is running on port 3000.'));
+application.listen(3000, () => console.log('🚀 Server is running on port 3000.'));
